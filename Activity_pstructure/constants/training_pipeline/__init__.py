f288461260459460bfef9660cba3984f36d4a41d
Saket Lambe <saket.lambe@gmail.com>
2024-11-19 21:09:01 +0000

    Data Transformation Completed

--- Activity_pstructure/constants/training_pipeline/__init__.py
+++ Activity_pstructure/constants/training_pipeline/__init__.py
@@ -31,5 +31,22 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid" 
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report" 
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report. yam1"
+PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
 
 SCHEMA_FILE_PATH =os.path.join("Data_schema", "schema.yaml")
+
+"Data Transformation VAR NAME"
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+## KNN imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Activity_pstructure.components.data_injestion import DataIngestion
 from Activity_pstructure.components.data_validation import DataValidation
-
+from Activity_pstructure.components.data_transformation import DataTransformation
 from Activity_pstructure.exception.exception import ActivityException
 from Activity_pstructure.logging.logger import logging
-from Activity_pstructure.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from Activity_pstructure.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from Activity_pstructure.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -22,7 +22,12 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(" data_validation_artifact")
-        
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data Transformation Started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation Completed")
 
     except Exception as e:
         raise ActivityException(e,sys)
