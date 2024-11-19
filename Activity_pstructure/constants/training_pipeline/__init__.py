74e8d5ad0c16290e4965c245e1846ee064e59689
Saket Lambe <saket.lambe@gmail.com>
2024-11-19 16:20:21 +0000

    Validation Completed

--- Activity_pstructure/constants/training_pipeline/__init__.py
+++ Activity_pstructure/constants/training_pipeline/__init__.py
@@ -15,10 +15,21 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 
-"Data Ingestion related constant DATA_INGESTION VAR NAME"
+"Data Ingestion VAR NAME"
+
 DATA_INGESTION_COLLECTION_NAME: str = "ActivityData" 
 DATA_INGESTION_DATABASE_NAME: str = "SDB" 
 DATA_INGESTION_DIR_NAME: str = "data_ingestion" 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store" 
 DATA_INGESTION_INGESTED_DIR: str = "ingested" 
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+"Data Vaildation VAR NAME"
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation" 
+DATA_VALIDATION_VALID_DIR: str = "validated" 
+DATA_VALIDATION_INVALID_DIR: str = "invalid" 
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report" 
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report. yam1"
+
+SCHEMA_FILE_PATH =os.path.join("Data_schema", "schema.yaml")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Activity_pstructure.components.data_injestion import DataIngestion
+from Activity_pstructure.components.data_validation import DataValidation
+
 from Activity_pstructure.exception.exception import ActivityException
 from Activity_pstructure.logging.logger import logging
-from Activity_pstructure.entity.config_entity import DataIngestionConfig
+from Activity_pstructure.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from Activity_pstructure.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -12,7 +14,15 @@ if __name__=="__main__":
         data_ingestion= DataIngestion(dataingestionconfig)
         logging.info("Initiate Data Ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Initiate Data Ingestion")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(" data_validation_artifact")
+        
 
     except Exception as e:
         raise ActivityException(e,sys)
