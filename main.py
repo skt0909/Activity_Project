c283a8ebb4fca5c6e6fe6635cb8ead45ee26d45f
Saket Lambe <saket.lambe@gmail.com>
2024-11-23 23:07:21 +0000

    main.py updated

--- main.py
+++ main.py
@@ -7,45 +7,7 @@ from Activity_pstructure.entity.config_entity import TrainingPipelineConfig
 from Activity_pstructure.components.data_transformation import DataTransformation, DataTransformationConfig
 from Activity_pstructure.components.model_trainer import ModelTrainer, ModelTrainerConfig
 import sys
-"""
-if __name__=="__main__":
-    try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion= DataIngestion(dataingestionconfig)
-        logging.info("Initiate Data Ingestion")
 
-        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-        logging.info("Data Initiation Completed")
-
-        print(data_ingestion_artifact)
-        data_validation_config=DataValidationConfig(trainingpipelineconfig)
-        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
-        logging.info("Initiate Data Ingestion")
-
-        data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("Data Validation Completed")
-        print(" data_validation_artifact")
-
-        train_data_path = data_ingestion_artifact.train_file_path
-        test_data_path = data_ingestion_artifact.test_file_path
-
-        data_transformation = DataTransformation()
-        
-        
-        train_arr,test_arr=data_transformation_artifact = data_transformation.initiate_data_transformation(
-            train_data_path, test_data_path
-        }
-      
-        logging.info("Data Transformation Completed")
-
-        modeltrainer=ModelTrainer()
-        print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-        
-
-    except Exception as e:
-        raise ActivityException(e,sys)
-"""
 if __name__ == "__main__":
     try:
         # Setup configurations and objects
